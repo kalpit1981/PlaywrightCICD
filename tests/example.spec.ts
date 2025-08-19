e8d2e6aaf308e0dfea0c43c7dcf9adf09251d2b5
Kalpit <kalpit.karapurkar@gmail.com>
2025-08-19 22:02:04 +1000

    failed test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -14,5 +14,5 @@ test('Test 2:- get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installations' })).toBeVisible();
 });
